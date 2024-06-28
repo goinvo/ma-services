790a6fe2e1c25ddaeee885f76e6c8cc30cde58df
Michelle Bourdon <michelle@bourdons.org>
2024-06-28 13:51:21 -0400

    eligability button

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 google.charts.load('current', {'packages':['treemap']});
-google.charts.setOnLoadCallback(drawChart);
+google.charts.setOnLoadCallback(() => loadSheet('all'));
 
 const itemDescriptions = {
     'MassHealth': 'Description for Healthcare services',
@@ -8,8 +8,25 @@ const itemDescriptions = {
     // Add more items and descriptions as needed
 };
 
-function drawChart() {
-    var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/1nKfr0JKhdzhHCMK1ilWhLRfPzgBd5fbpZTtXtOdglsg/gviz/tq?sheet=Sheet1');
+let chart;
+let options = {
+    minColor: '#D3EFF4', // Light blue
+    midColor: '#9BC8CF', // Medium blue
+    maxColor: '#007385', // Dark blue
+    headerHeight: 25,
+    fontColor: 'black',
+    showScale: true,
+    useWeightedAverageForAggregation: true
+};
+
+function loadSheet(sheetType) {
+    let sheetUrl;
+    if (sheetType === 'all') {
+        sheetUrl = 'https://docs.google.com/spreadsheets/d/1nKfr0JKhdzhHCMK1ilWhLRfPzgBd5fbpZTtXtOdglsg/gviz/tq?sheet=Sheet1';
+    } else if (sheetType === 'eligibility') {
+        sheetUrl = 'https://docs.google.com/spreadsheets/d/18zHBxhj_UAkdqEzQsVTFJ5siOZBqMEwjdA6Z0kPumxM/gviz/tq?sheet=Sheet1';
+    }
+    var query = new google.visualization.Query(sheetUrl);
 
     query.send(function(response) {
         if (response.isError()) {
@@ -18,28 +35,19 @@ function drawChart() {
         }
 
         var data = response.getDataTable();
-        var chart = new google.visualization.TreeMap(document.getElementById('chart_div'));
+        if (!chart) {
+            chart = new google.visualization.TreeMap(document.getElementById('chart_div'));
+        }
 
         google.visualization.events.addListener(chart, 'select', function() {
             var selection = chart.getSelection();
             if (selection.length > 0) {
                 var item = data.getValue(selection[0].row, 0);
-                console.log('Selected item:', item); // Debug log
                 var description = itemDescriptions[item] || 'No description available for ' + item;
                 document.getElementById('info').innerText = description;
                 chart.setSelection([]); // Clear the selection to prevent zooming
             }
         });
-
-        var options = {
-            minColor: '#D3EFF4', // Light blue
-            midColor: '#9BC8CF', // Medium blue
-            maxColor: '#007385', // Dark blue
-            headerHeight: 25,
-            fontColor: 'black',
-            showScale: true,
-            useWeightedAverageForAggregation: true
-        };
 
         chart.draw(data, options);
     });
