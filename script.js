5c3417105f010c9e134410eb35b3278188e6a59d
Michelle Bourdon <michelle@bourdons.org>
2024-06-28 12:52:25 -0400

    updates

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
-// Load the Visualization API and the treemap package.
 google.charts.load('current', {'packages':['treemap']});
-
-// Set a callback to run when the Google Visualization API is loaded.
 google.charts.setOnLoadCallback(drawChart);
 
 function drawChart() {
-    // Replace the URL below with the URL of your Google Sheet
     var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/1nKfr0JKhdzhHCMK1ilWhLRfPzgBd5fbpZTtXtOdglsg/gviz/tq?sheet=Sheet1');
 
-    query.send(handleQueryResponse);
-}
+    query.send(function(response) {
+        if (response.isError()) {
+            console.error('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+            return;
+        }
 
-function handleQueryResponse(response) {
-    if (response.isError()) {
-        console.error('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
-        return;
-    }
+        var data = response.getDataTable();
+        var chart = new google.visualization.TreeMap(document.getElementById('chart_div'));
 
-    var data = response.getDataTable();
+        google.visualization.events.addListener(chart, 'select', function() {
+            var selection = chart.getSelection();
+            if (selection.length > 0) {
+                var item = data.getValue(selection[0].row, 0);
+                document.getElementById('info').innerText = 'You selected: ' + item;
+            }
+        });
 
-    var options = {
-        minColor: '#f00',
-        midColor: '#ddd',
-        maxColor: '#0d0',
-        headerHeight: 15,
-        fontColor: 'black',
-        showScale: true
-    };
-
-    var chart = new google.visualization.TreeMap(document.getElementById('chart_div'));
-    chart.draw(data, options);
+        chart.draw(data, {
+            minColor: '#f00',
+            midColor: '#ddd',
+            maxColor: '#0d0',
+            headerHeight: 15,
+            fontColor: 'black',
+            showScale: true
+        });
+    });
 }
