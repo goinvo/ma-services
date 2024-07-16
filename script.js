604cd57ac16f63cdcac8d17550b91082f4b4a483
Michelle Bourdon <michelle@bourdons.org>
2024-07-16 13:51:22 -0400

    v3

--- script.js
+++ script.js
@@ -1,27 +1,30 @@
-const itemDescriptions = {
-    'MassHealth': 'Massachusetts Health Insurance',
-    'TAFDC': 'Transitional Aid to Families with Dependent Children',
-    'EAEDC': 'Emergency Aid to the Elderly, Disabled, and Children',
-    'SNAP': 'Supplemental Nutrition Assistance Program',
-    'LIHEAP': 'Low Income Home Energy Assistance Program',
-    'SSDI': 'Social Security Disability Insurance',
-    'SSI': 'Supplemental Security Income'
-    // Add more items and descriptions as needed
-};
+let currentData; // Track the current hierarchical data
+let currentView = 'tree'; // Track the current view
+let currentFile = 'services.json'; // Track the current data file
 
 // Load data from JSON file
 function loadData(jsonFile) {
     fetch(jsonFile + '?_=' + new Date().getTime()) // Append a unique query string to disable caching
         .then(response => response.json())
         .then(data => {
-            const hierarchyData = buildHierarchy(data); // Convert flat data to hierarchical
-            drawTreeMap(hierarchyData); // Draw the TreeMap
-            window.addEventListener('resize', () => drawTreeMap(hierarchyData)); // Redraw tree map on window resize
+            currentData = buildHierarchy(data); // Convert flat data to hierarchical
+            if (currentView === 'tree') {
+                drawTreeMap(currentData, true);
+            } else {
+                drawTable(currentData);
+            }
+            window.addEventListener('resize', () => {
+                if (currentView === 'tree') {
+                    drawTreeMap(currentData, false);
+                } else {
+                    drawTable(currentData);
+                }
+            }); // Redraw tree map or table on window resize
         })
         .catch(error => console.error('Error fetching data:', error));
 }
 
-// Convert flat data to hierarchical format
+// Convert flat data to hierarchical format (needed for tree map)
 function buildHierarchy(data) {
     const root = { name: "Services", children: [] };
     Object.keys(data).forEach(key => {
@@ -35,31 +38,35 @@ function buildHierarchy(data) {
     return root;
 }
 
-// Function to update the header text
+// Updates header text
 function updateHeader(title) {
-    document.getElementById('tree-map-header').innerHTML = `<button id="back-button" class="back-button" style="display: none;">&lt;</button> ${title}`;
+    document.getElementById('tree-map-header').innerHTML = `<button id="back-button" class="back-button" style="display: ${currentFile === 'other.json' ? 'inline-block' : 'none'};">&lt;</button> ${title}`;
     document.getElementById('back-button').addEventListener('click', function() {
         loadData('services.json');
+        currentFile = 'services.json';
         updateHeader("All Massachusetts Services");
         document.getElementById('back-button').style.display = 'none';
     });
 }
 
-// Function to draw tree map
-function drawTreeMap(data) {
+// Draws tree map
+function drawTreeMap(data, transition = false) {
+    const chartDiv = document.getElementById('d3_chart_div');
+    chartDiv.style.display = 'block';
+    chartDiv.style.opacity = 0;  // Added to set initial opacity to 0
+    document.getElementById('table_div').style.display = 'none';
+    highlightSelectedViewButton('tree');
+
     const container = document.getElementById('d3_chart_div');
     const width = container.clientWidth;
     const height = container.clientHeight;
 
-    // Custom color scale using shades of blue
     const color = d3.scaleLinear()
         .domain([0, d3.max(data.children, d => d.value)])
-        .range(["#E6F7FA", "#BBDCE1"]); // Light blue to dark blue
+        .range(["#E6F7FA", "#BBDCE1"]);
 
-    // Number format with commas
     const format = d3.format(",");
 
-    // Compute the layout with resquarify method
     const root = d3.treemap()
         .tile(d3.treemapResquarify)
         .size([width, height])
@@ -69,41 +76,23 @@ function drawTreeMap(data) {
             .sum(d => d.value)
             .sort((a, b) => b.value - a.value));
 
-    // Create SVG container
     const svg = d3.select("#d3_chart_div")
-        .html("") // Clear previous contents
+        .html("")
         .append("svg")
         .attr("viewBox", `0 0 ${width} ${height}`)
         .attr("width", "100%")
         .attr("height", "100%")
         .attr("style", "font: 10px sans-serif;");
 
-    // Define padding values
-    const paddingTop = 10;
-    const paddingLeft = 10;
-
-    // Add a group for each leaf node
     const node = svg.selectAll("g")
         .data(root.leaves())
         .join("g")
         .attr("transform", d => `translate(${d.x0},${d.y0})`)
-        .on("click", function(event, d) {
-            if (d.data.name === "Other") {
-                loadData('other.json');
-                updateHeader("Other Services");
-                document.getElementById('back-button').style.display = 'block';
-            } else {
-                const description = itemDescriptions[d.data.name] || 'No description available';
-                const formattedDescription = `${d.data.name} - ${description}\n${format(d.value)} individuals enrolled`;
-                document.getElementById('info').innerText = formattedDescription; // Show description on click
-            }
-        });
+        .on("click", (event, d) => handleNodeClick(d));
 
-    // Add title (tooltip) for each node
     node.append("title")
         .text(d => `${d.ancestors().reverse().map(d => d.data.name).join("/")}\n${d.value}`);
 
-    // Add rectangles for each node
     node.append("rect")
         .attr("id", d => (d.leafUid = d.data.name))
         .attr("fill", d => color(d.value))
@@ -111,7 +100,15 @@ function drawTreeMap(data) {
         .attr("width", d => d.x1 - d.x0)
         .attr("height", d => d.y1 - d.y0);
 
-    // Add text for each node
+    if (transition) {
+        d3.select("#d3_chart_div")
+            .transition()  // Added to apply transition
+            .duration(750)
+            .style("opacity", 1);  // Added to transition opacity to 1
+    } else {
+        chartDiv.style.opacity = 1;
+    }
+
     node.append("text")
         .attr("x", d => (d.x1 - d.x0) / 2)
         .attr("y", d => (d.y1 - d.y0) / 2)
@@ -119,11 +116,10 @@ function drawTreeMap(data) {
         .attr("class", "node-text")
         .style("text-anchor", "middle")
         .text(d => d.data.name)
-        .call(wrap, function(d) { return d.x1 - d.x0 - 2 * paddingLeft; });
+        .call(wrapText, function(d) { return d.x1 - d.x0 - 2 * paddingLeft; });
 
-    // Add enrolled text for large nodes
-    node.filter(d => d.value >= 250000)          // check size for determining if a box gets data
-        .append("text")                             
+    node.filter(d => d.value >= 250000)
+        .append("text")
         .attr("x", d => (d.x1 - d.x0) / 2)
         .attr("y", d => (d.y1 - d.y0) / 2 + 20)
         .attr("fill", "black")
@@ -132,23 +128,81 @@ function drawTreeMap(data) {
         .text(d => `${format(d.value)} enrolled`);
 }
 
-// Function to wrap text within a given width
-function wrap(text, width) {
-    text.each(function () {
+
+// Draws table
+function drawTable(data) {
+    document.getElementById('d3_chart_div').style.display = 'none';
+    document.getElementById('table_div').style.display = 'block';
+    highlightSelectedViewButton('table');
+
+    const flatData = data.children.map(d => ({
+        name: d.name,
+        value: d.value
+    }));
+
+    const tableDiv = d3.select("#table_div");
+    tableDiv.html(""); // Clear previous contents
+
+    const table = tableDiv.append("table").attr("class", "data-table");
+    const thead = table.append("thead");
+    const tbody = table.append("tbody");
+
+    thead.append("tr")
+        .selectAll("th")
+        .data(["Service Name", "Enrolled"])
+        .enter()
+        .append("th")
+        .text(d => d);
+
+    const rows = tbody.selectAll("tr")
+        .data(flatData)
+        .enter()
+        .append("tr")
+        .on("click", (event, d) => {
+            if (d.name === 'Other') {
+                loadData('other.json');
+                currentFile = 'other.json';
+                updateHeader("Other Services");
+            }
+        });
+
+    rows.selectAll("td")
+        .data(d => [d.name, d.value])
+        .enter()
+        .append("td")
+        .text(d => d);
+    
+    tableDiv.transition()
+        .duration(750)
+        .style("opacity", 1);
+}
+
+function handleNodeClick(d) {
+    if (d.data.name === "Other") {
+        zoom(d); // Initiate zoom transition
+        setTimeout(() => {
+            loadData('other.json');
+            currentFile = 'other.json';
+            updateHeader("Other Services");
+        }, 750); // Load new data after the zoom transition
+    }
+}
+
+
+
+// Text wrapping
+function wrapText(text, width) {
+    text.each(function() {
         var text = d3.select(this),
             words = text.text().split(/\s+/).reverse(),
             word,
             line = [],
             lineNumber = 0,
-            lineHeight = 1.1, // ems
+            lineHeight = 1.1,
             x = text.attr("x"),
             y = text.attr("y"),
-            dy = 0, //parseFloat(text.attr("dy")),
-            tspan = text.text(null)
-                        .append("tspan")
-                        .attr("x", x)
-                        .attr("y", y)
-                        .attr("dy", dy + "em");
+            dy = 0,
+            tspan = text.text(null).append("tspan").attr("x", x).attr("y", y).attr("dy", dy + "em");
         while (word = words.pop()) {
             line.push(word);
             tspan.text(line.join(" "));
@@ -156,14 +210,9 @@ function wrap(text, width) {
                 line.pop();
                 tspan.text(line.join(" "));
                 line = [word];
-                tspan = text.append("tspan")
-                            .attr("x", x)
-                            .attr("y", y)
-                            .attr("dy", ++lineNumber * lineHeight + dy + "em")
-                            .text(word);
+                tspan = text.append("tspan").attr("x", x).attr("y", y).attr("dy", ++lineNumber * lineHeight + dy + "em").text(word);
             }
         }
-        // Center the text vertically within the box
         const bbox = text.node().getBBox();
         const textHeight = bbox.height;
         const boxHeight = d3.select(text.node().parentNode).select('rect').attr('height');
@@ -171,72 +220,112 @@ function wrap(text, width) {
     });
 }
 
+// Highlight selected view button
+function highlightSelectedViewButton(view) {
+    currentView = view;
+    document.querySelectorAll('.view-button').forEach(button => {
+        button.classList.remove('selected');
+    });
+    if (view === 'tree') {
+        document.getElementById('tree-view-button').classList.add('selected');
+        document.getElementById('tree-view-button-mobile').classList.add('selected');
+    } else {
+        document.getElementById('table-view-button').classList.add('selected');
+        document.getElementById('table-view-button-mobile').classList.add('selected');
+    }
+}
+// Highlight selected filter button
+function highlightSelectedFilterButton(filter) {
+    document.querySelectorAll('.filter-button').forEach(button => {
+        button.classList.remove('selected');
+    });
+    if (filter === 'all') {
+        document.getElementById('all-services-button').classList.add('selected');
+        document.getElementById('all-services-button-mobile').classList.add('selected');
+    } else if (filter === 'core') {
+        document.getElementById('eligibility-button').classList.add('selected');
+        document.getElementById('eligibility-button-mobile').classList.add('selected');
+    }
+}
 
-// Event listeners 
-// Load initial data when the document is ready
+// Initial load -- all services in tree view
 document.addEventListener("DOMContentLoaded", function() {
     loadData('services.json');
     updateHeader("All Massachusetts Services");
+    setupEventListeners();
+    highlightSelectedFilterButton("all");
+    highlightSelectedViewButton("tree");
 });
 
-// Event listeners for buttons (Mobile)
-document.getElementById('back-button').addEventListener('click', function() {
-    loadData('services.json');
-    updateHeader("All Massachusetts Services");
-    document.getElementById('back-button').style.display = 'none';
-});
+// Event listeners 
+function setupEventListeners() {
+    const actions = [
+        { id: 'back-button', file: 'services.json', header: 'All Massachusetts Services', showBackButton: false },
+        { id: 'all-services-button', file: 'services.json', header: 'All Massachusetts Services', showBackButton: false },
+        { id: 'eligibility-button', file: 'elig.json', header: 'Eligibility Services', showBackButton: true },
+        { id: 'see-code-button', action: () => window.open('https://github.com/goinvo/ma-services', '_blank') },
+        { id: 'tree-view-button', action: () => drawTreeMap(currentData) },
+        { id: 'table-view-button', action: () => drawTable(currentData) },
+        { id: 'all-services-button-mobile', file: 'services.json', header: 'All Massachusetts Services', showBackButton: false },
+        { id: 'eligibility-button-mobile', file: 'elig.json', header: 'Eligibility Services', showBackButton: true },
+        { id: 'tree-view-button-mobile', action: () => drawTreeMap(currentData) },
+        { id: 'table-view-button-mobile', action: () => drawTable(currentData) },
+        { id: 'see-code-button-mobile', action: () => window.open('https://github.com/goinvo/ma-services', '_blank') }
+    ];
 
-document.getElementById('all-services-button').addEventListener('click', function() {
-    loadData('services.json');
-    updateHeader("All Massachusetts Services");
-    document.getElementById('back-button').style.display = 'none';
-    toggleMenu();
-});
-document.getElementById('eligibility-button').addEventListener('click', function() {
-    loadData('elig.json');
-    updateHeader("Eligibility Services");
-    document.getElementById('back-button').style.display = 'inline-block';
-    toggleMenu();
-});
-document.getElementById('other-button').addEventListener('click', function() {
-    loadData('other.json');
-    updateHeader("Other Services");
-    document.getElementById('back-button').style.display = 'inline-block';
-    toggleMenu();
-});
-document.getElementById('see-code-button').addEventListener('click', function() {
-    window.open('https://github.com/goinvo/ma-services', '_blank');
-});
-
-
-// Event listeners for buttons (Desktop)
-document.getElementById('all-services-button-desktop').addEventListener('click', function() {
-    loadData('services.json');
-    updateHeader("All Massachusetts Services");
-    document.getElementById('back-button').style.display = 'inline-block';
-});
-document.getElementById('eligibility-button-desktop').addEventListener('click', function() {
-    loadData('elig.json');
-    updateHeader("Eligibility Services");
-    document.getElementById('back-button').style.display = 'inline-block';
-});
-document.getElementById('other-button-desktop').addEventListener('click', function() {
-    loadData('other.json');
-    updateHeader("Other Services");
-    document.getElementById('back-button').style.display = 'inline-block';
-});
-document.getElementById('see-code-button-desktop').addEventListener('click', function() {
-    window.open('https://github.com/goinvo/ma-services', '_blank');
-});
-
-
-
-// Toggle mobile menu 
-function toggleMenu() {
-    const menu = document.getElementById("mobileMenu");
-    if (menu.style.display === "flex") {
-        menu.style.display = "none";
-    } else {
-        menu.style.display = "flex";
-    }
+    actions.forEach(({ id, file, header, showBackButton, action }) => {
+        const element = document.getElementById(id);
+        if (element) {
+            element.addEventListener('click', () => {
+                if (action) {
+                    action();
+                } else {
+                    loadData(file);
+                    currentFile = file;
+                    updateHeader(header);
+                    document.getElementById('back-button').style.display = file === 'other.json' ? 'inline-block' : 'none';
+                    if (id.includes('all-services-button')) {
+                        highlightSelectedFilterButton('all');
+                    } else if (id.includes('eligibility-button')) {
+                        highlightSelectedFilterButton('core');
+                    }
+                }
+            });
+        }
+    });
 }
+
+// Zoom into other transition -- really not necessary
+function zoom(d) {
+    const chartDiv = document.getElementById('d3_chart_div');
+    const width = chartDiv.clientWidth;
+    const height = chartDiv.clientHeight;
+
+    const x = d3.scaleLinear()
+        .domain([d.x0, d.x1])
+        .range([0, width]);
+    
+    const y = d3.scaleLinear()
+        .domain([d.y0, d.y1])
+        .range([0, height]);
+
+    const svg = d3.select("#d3_chart_div svg");
+
+    const t = svg.transition()
+        .duration(750)
+        .attr("viewBox", `0 0 ${width} ${height}`)
+        .attr("width", "100%")
+        .attr("height", "100%");
+
+    svg.selectAll("g")
+        .transition(t)
+        .attr("transform", d => `translate(${x(d.x0)},${y(d.y0)})`);
+
+    svg.selectAll("rect")
+        .transition(t)
+        .attr("x", d => x(d.x0))
+        .attr("y", d => y(d.y0))
+        .attr("width", d => x(d.x1) - x(d.x0))
+        .attr("height", d => y(d.y1) - y(d.y0));
+}
+
